30a647e75f2392a3858f342ae1ae7bd5cff4976e
Kyle Vaughn <kcvaughn75@gmail.com>
2020-02-06 15:51:33 -0500

    initial commit

---
+++ Midterm_Fitness_Center/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Midterm_Fitness_Center
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

e440ea8ccd9ed99e75392e60c354f6f3a3cf8d76
Kyle Vaughn <kcvaughn75@gmail.com>
2020-02-06 17:29:18 -0500

    added club class and created parent member class

---
+++ Midterm_Fitness_Center/Club.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Midterm_Fitness_Center
+{
+    class Club
+    {
+    }
+}
---
+++ Midterm_Fitness_Center/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Midterm_Fitness_Center
+{
+    abstract class Member
+    {   
+        //Fields
+        private int id;
+        private string lastName;
+        private string firstName;
+        private double fees;
+
+        //Properties
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public double Fees { get; set; }
+
+        //abstract method will be defined by child classes
+        public abstract void CheckIn(Club club);
+
+        virtual public void AddMember()
+        {
+
+        }
+        
+       
+
+    }
+}

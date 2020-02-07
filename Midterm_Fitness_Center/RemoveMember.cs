fede6409822a4e10cadf13a0efb8d19f5cbe2643
Erwin <coronele@hotmail.com>
2020-02-07 06:45:47 -0500

    RemoveUser method built

--- Midterm_Fitness_Center/RemoveMember.cs
+++ Midterm_Fitness_Center/RemoveMember.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static Midterm_Fitness_Center.DisplayMember;
 
 namespace Midterm_Fitness_Center
 {
@@ -8,7 +9,30 @@ namespace Midterm_Fitness_Center
     {
         public static void RmvMember(List<Member> memberList)
         {
+            int memberToDelete;
+            // Find member to remove
+            memberToDelete = DispMember(memberList);
+            if (memberToDelete == -1)
+            {
+                Console.WriteLine("Could not remove member.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                // VALIDATION NEEDED HERE
+                Console.WriteLine($"\n\nAre you sure you want to remove {memberList[memberToDelete].FirstName} {memberList[memberToDelete].LastName} from the system? [y/n]");
+                string deleteUser = Console.ReadLine();    // Other options?
+                if (deleteUser == "y")
+                {
+                    memberList.RemoveAt(memberToDelete);
+                    Console.WriteLine("User removed from system.\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("Remove user cancelled.\n");
+                }
 
+            }
         }
     }
 }
